e457afbc141186b4e5eb7ee24ef65f549f9a1a42
samidejen <bold.3453a@gmail.com>
2024-12-27 20:50:21 +0300

    Update mongoose.ts

--- src/loaders/mongoose.ts
+++ src/loaders/mongoose.ts
@@ -3,8 +3,6 @@ import * as mongoose from "mongoose";
 import config from "../config/index";
 
 export default async () => {
-  const connection = await mongoose.connect(
-    "mongodb+srv://Sami:Sami@1994@cluster0.0nmsh.mongodb.net/"
-  );
+  const connection = await mongoose.connect(config.atlas);
   return connection.connection.db;
 };
